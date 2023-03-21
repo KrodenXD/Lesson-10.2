0f72c871784193443f423f0b133f301f48a510a7
alexc <alexc@SONY-VAIO>
2023-03-21 22:38:31 +0400

    Add project files.

---
+++ Lesson 10.2/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._2
+{
+    class Methods
+    {
+        public int Factorial(int a)
+        {
+            if (a == 0)
+            {
+                return 1;
+            }
+            return a * Factorial(a - 1);
+        }
+        public decimal? Multiply(decimal? a, decimal? b, decimal? c, decimal? d)
+        {
+            return a * b * c * d;
+        }
+    }
+}
---
+++ Lesson 10.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            //Console.WriteLine("Input :");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Factorial of " + a + " = " + methods.Factorial(a));
+            Console.WriteLine(methods.Multiply(null,null,null,10));
+        }
+    }
+}
